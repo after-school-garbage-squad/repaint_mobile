a669ade76b4a282b4d8eefbd9693158378642a2f
Hideki Tanaka <work@tuxsnct.com>
2023-08-11 02:35:09 +0900

    feat: IntroductionSettingsScreen almost completed

--- lib/features/introduction/presentation/screens/introduction_settings_screen.dart
+++ lib/features/introduction/presentation/screens/introduction_settings_screen.dart
@@ -1,5 +1,6 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:repaint_mobile/features/common/presentation/widgets/elevated_text_button.dart';
 import 'package:repaint_mobile/features/common/presentation/widgets/flat_icon_button.dart';
 
 @RoutePage()
@@ -15,6 +16,27 @@ class IntroductionSettingsScreen extends StatelessWidget {
           icon: Icons.chevron_left,
         ),
         backgroundColor: Theme.of(context).colorScheme.background,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          children: [
+            Align(
+              alignment: Alignment.centerLeft,
+              child:
+                  Text("管理者設定", style: Theme.of(context).textTheme.titleLarge),
+            ),
+            const SizedBox(height: 16),
+            ElevatedTextButton(
+              onPressed: () {},
+              text: "管理者としてログイン",
+              colors: const ElevatedTextButtonColors(
+                backgroundColor: Colors.white,
+                borderColor: Colors.white,
+              ),
+            )
+          ],
+        ),
       ),
       backgroundColor: Theme.of(context).colorScheme.background,
     );
