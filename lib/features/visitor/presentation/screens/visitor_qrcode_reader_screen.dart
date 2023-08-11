dcdf7f4dab0658ea3fa0574586498bf57bfe0e06
Hideki Tanaka <work@tuxsnct.com>
2023-08-11 13:05:02 +0900

    feat: add VisitorQRCodeReaderScreen

---
+++ lib/features/visitor/presentation/screens/visitor_qrcode_reader_screen.dart
@@ -0,0 +1,44 @@
+import 'package:auto_route/auto_route.dart';
+import 'package:flutter/material.dart';
+import 'package:repaint_mobile/features/common/presentation/widgets/flat_icon_button.dart';
+
+@RoutePage()
+class VisitorQRCodeReaderScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: FlatIconButton(
+          onPressed: context.popRoute,
+          icon: Icons.chevron_left,
+        ),
+        backgroundColor: Colors.transparent,
+      ),
+      body: Column(
+        children: [
+          // TODO: カメラを実装する
+          const Expanded(
+            child: Placeholder(),
+          ),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 0.0),
+            child: Column(
+              children: [
+                Text(
+                  'スポットのQRコードを読み込んでください',
+                  style: Theme.of(context).textTheme.bodyMedium,
+                  textAlign: TextAlign.center,
+                ),
+              ],
+            ),
+          ),
+          ConstrainedBox(
+            constraints: const BoxConstraints(minHeight: 96.0),
+          ),
+        ],
+      ),
+      backgroundColor: Theme.of(context).colorScheme.background,
+      extendBodyBehindAppBar: true,
+    );
+  }
+}
