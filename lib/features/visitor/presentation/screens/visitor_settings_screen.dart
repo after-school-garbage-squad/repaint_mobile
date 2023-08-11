bfaddab627eb72ca276c36f5302d4310e41e3e10
Hideki Tanaka <work@tuxsnct.com>
2023-08-11 16:58:24 +0900

    feat: add notification settings in VisitorSettingsScreen

--- lib/features/visitor/presentation/screens/visitor_settings_screen.dart
+++ lib/features/visitor/presentation/screens/visitor_settings_screen.dart
@@ -2,6 +2,7 @@ import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:repaint_mobile/features/common/presentation/widgets/flat_icon_button.dart';
 import 'package:repaint_mobile/features/common/presentation/widgets/wide_elevated_button.dart';
+import 'package:repaint_mobile/features/visitor/presentation/widgets/settings_toggle_element.dart';
 
 @RoutePage()
 class VisitorSettingsScreen extends StatelessWidget {
@@ -20,14 +21,32 @@ class VisitorSettingsScreen extends StatelessWidget {
       body: Padding(
         padding: const EdgeInsets.all(16),
         child: Column(
-          // TODO: 未実装の項目を実装する
           children: [
             Align(
               alignment: Alignment.centerLeft,
               child:
                   Text("通知設定", style: Theme.of(context).textTheme.titleLarge),
             ),
-            const SizedBox(height: 16),
+            const SizedBox(height: 12),
+            // TODO: 実際の通知設定を実装する
+            SettingsToggleElement(
+              title: "スポットに入った際の通知",
+              value: true,
+              onChanged: (value) {},
+            ),
+            const SizedBox(height: 12),
+            SettingsToggleElement(
+              title: "イベントからのお知らせ",
+              value: true,
+              onChanged: (value) {},
+            ),
+            const SizedBox(height: 12),
+            SettingsToggleElement(
+              title: "その他の通知",
+              value: true,
+              onChanged: (value) {},
+            ),
+            const SizedBox(height: 32),
             Align(
               alignment: Alignment.centerLeft,
               child: Text(
@@ -35,7 +54,7 @@ class VisitorSettingsScreen extends StatelessWidget {
                 style: Theme.of(context).textTheme.titleLarge,
               ),
             ),
-            const SizedBox(height: 16),
+            const SizedBox(height: 12),
             // TODO: ログアウト機能を実装する
             WideElevatedButton.withTemplate(
               onPressed: () {},
