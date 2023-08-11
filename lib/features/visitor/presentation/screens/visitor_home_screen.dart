432b118ba10f39fc1dda6bb37b7fe97359889e4c
Hideki Tanaka <work@tuxsnct.com>
2023-08-11 13:04:51 +0900

    feat: add VisitorHomeScreen

---
+++ lib/features/visitor/presentation/screens/visitor_home_screen.dart
@@ -0,0 +1,65 @@
+import 'package:auto_route/auto_route.dart';
+import 'package:flutter/material.dart';
+import 'package:repaint_mobile/features/common/presentation/widgets/elevated_text_button.dart';
+import 'package:repaint_mobile/features/common/presentation/widgets/flat_icon_button.dart';
+
+@RoutePage()
+class VisitorHomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        actions: [
+          FlatIconButton(
+            onPressed: () {
+              context.navigateNamedTo("settings");
+            },
+            icon: Icons.settings,
+          )
+        ],
+        backgroundColor: Theme.of(context).colorScheme.background,
+      ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(16.0, 32.0, 16.0, 0.0),
+          child: Column(
+            children: [
+              ConstrainedBox(
+                // TODO: 実際の画像のサイズに合わせる
+                constraints: const BoxConstraints(maxHeight: 480.0),
+                child: const AspectRatio(
+                  aspectRatio: 1,
+                  // TODO: 画像を設定する
+                  child: Placeholder(fallbackWidth: double.infinity),
+                ),
+              ),
+              const SizedBox(height: 16.0),
+              ElevatedTextButton(
+                onPressed: () {
+                  context.navigateNamedTo("qrcode_reader");
+                },
+                text: "QRコードの読み取り",
+                colors: const ElevatedTextButtonColors(
+                  backgroundColor: Colors.white,
+                ),
+              ),
+              const SizedBox(height: 16.0),
+              const Spacer(),
+              ElevatedTextButton(
+                onPressed: () {
+                  context.navigateNamedTo("explain");
+                },
+                text: "進む",
+              ),
+              ConstrainedBox(
+                constraints: const BoxConstraints(minHeight: 96.0),
+              ),
+            ],
+          ),
+        ),
+      ),
+      backgroundColor: Theme.of(context).colorScheme.background,
+    );
+  }
+}
