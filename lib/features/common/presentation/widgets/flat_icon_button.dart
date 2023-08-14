ebe1fa6f561dc80c3dbb23cd596de6f710a17b4c
Hideki Tanaka <work@tuxsnct.com>
2023-08-15 02:32:50 +0900

    feat: make FlatIconButton more customizable

--- lib/features/common/presentation/widgets/flat_icon_button.dart
+++ lib/features/common/presentation/widgets/flat_icon_button.dart
@@ -1,17 +1,51 @@
 import 'package:flutter/material.dart';
 
+class FlatIconButtonColors {
+  const FlatIconButtonColors({
+    this.borderColor,
+    this.backgroundColor,
+  });
+
+  final Color? borderColor;
+  final Color? backgroundColor;
+}
+
 class FlatIconButton extends StatelessWidget {
   const FlatIconButton({
     super.key,
     required this.onPressed,
     required this.icon,
+    this.iconSize,
+    this.colors,
+    this.elevation,
   });
 
   final VoidCallback onPressed;
   final IconData icon;
+  final double? iconSize;
+  final FlatIconButtonColors? colors;
+  final double? elevation;
 
   @override
   Widget build(BuildContext context) {
-    return IconButton(onPressed: onPressed, icon: Icon(icon), iconSize: 32.0);
+    return IconButton(
+      alignment: Alignment.center,
+      onPressed: onPressed,
+      icon: Icon(icon),
+      iconSize: iconSize ?? 32.0,
+      style: ButtonStyle(
+        backgroundColor: MaterialStateProperty.all<Color>(
+          colors?.backgroundColor ?? Colors.transparent,
+        ),
+        shape: MaterialStateProperty.all<OutlinedBorder>(
+          CircleBorder(
+            side: BorderSide(
+              color: colors?.borderColor ?? Colors.transparent,
+            ),
+          ),
+        ),
+        elevation: MaterialStateProperty.all<double>(elevation ?? 0.0),
+      ),
+    );
   }
 }
