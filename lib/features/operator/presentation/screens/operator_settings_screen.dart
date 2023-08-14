62a7289c61dfd0dc6510254099057cbaa4a4b750
Hideki Tanaka <work@tuxsnct.com>
2023-08-15 00:16:56 +0900

    feat: OperatorSettingsScreen almost completed

--- lib/features/operator/presentation/screens/operator_settings_screen.dart
+++ lib/features/operator/presentation/screens/operator_settings_screen.dart
@@ -1,6 +1,7 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:repaint_mobile/features/common/presentation/widgets/flat_icon_button.dart';
+import 'package:repaint_mobile/features/common/presentation/widgets/wide_elevated_button.dart';
 
 @RoutePage()
 class OperatorSettingsScreen extends StatelessWidget {
@@ -15,6 +16,31 @@ class OperatorSettingsScreen extends StatelessWidget {
           icon: Icons.chevron_left,
         ),
         backgroundColor: Theme.of(context).colorScheme.background,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          children: [
+            Align(
+              alignment: Alignment.centerLeft,
+              child: Text("アカウント設定",
+                  style: Theme.of(context).textTheme.titleLarge),
+            ),
+            const SizedBox(height: 16),
+            WideElevatedButton(
+              onPressed: () {
+                // TODO: ログアウト処理を実装する
+                context.navigateNamedTo("/introduction");
+              },
+              text: "ログアウト",
+              colors: const WideElevatedButtonColors(
+                backgroundColor: Colors.white,
+                borderColor: Colors.white,
+                textColor: Colors.red,
+              ),
+            )
+          ],
+        ),
       ),
       backgroundColor: Theme.of(context).colorScheme.background,
     );
