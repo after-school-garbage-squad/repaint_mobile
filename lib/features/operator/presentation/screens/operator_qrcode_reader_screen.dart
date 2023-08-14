6fce5c14eccbb2d83e8b5c4ac7baf7ef3ed6dc45
Hideki Tanaka <work@tuxsnct.com>
2023-08-15 02:04:01 +0900

    feat: OperatorQRCodeReaderScreen almost completed

--- lib/features/operator/presentation/screens/operator_qrcode_reader_screen.dart
+++ lib/features/operator/presentation/screens/operator_qrcode_reader_screen.dart
@@ -1,22 +1,54 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
-import 'package:repaint_mobile/features/common/presentation/widgets/flat_icon_button.dart';
+import 'package:repaint_mobile/features/common/presentation/widgets/camera_scaffold.dart';
 
 @RoutePage()
 class OperatorQRCodeReaderScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("QRコードリーダー"),
-        centerTitle: true,
-        leading: FlatIconButton(
-          onPressed: context.popRoute,
-          icon: Icons.chevron_left,
+    return CameraScaffold(
+      preview: Expanded(
+        child: Stack(
+          children: [
+            // TODO: カメラを実装する
+            const Placeholder(),
+            Positioned(
+              right: 14,
+              bottom: 8,
+              child: Container(
+                width: 100,
+                height: 48,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(8),
+                ),
+                child: Center(
+                  child: Text(
+                    "1枚目",
+                    style: Theme.of(context).textTheme.bodyMedium,
+                  ),
+                ),
+              ),
+            ),
+          ],
         ),
-        backgroundColor: Theme.of(context).colorScheme.background,
       ),
-      backgroundColor: Theme.of(context).colorScheme.background,
+      children: [
+        Padding(
+          padding: const EdgeInsets.only(top: 64.0),
+          child: Center(
+            child: Center(
+              child: Text(
+                "参加者のQRコードを読み取ってください",
+                style: Theme.of(context).textTheme.bodyLarge,
+              ),
+            ),
+          ),
+        ),
+        ConstrainedBox(
+          constraints: const BoxConstraints(minHeight: 96.0),
+        ),
+      ],
     );
   }
 }
