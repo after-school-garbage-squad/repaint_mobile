bb07e35607355234c948c418812a4b2e3da9bbac
Hideki Tanaka <work@tuxsnct.com>
2023-08-15 15:15:50 +0900

    feat: OperatorBeaconSettingsScreen almost completed

--- lib/features/operator/presentation/screens/operator_beacon_settings_screen.dart
+++ lib/features/operator/presentation/screens/operator_beacon_settings_screen.dart
@@ -1,22 +1,56 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
-import 'package:repaint_mobile/features/common/presentation/widgets/flat_icon_button.dart';
+import 'package:repaint_mobile/features/common/presentation/widgets/bottom_constrained_padding.dart';
+import 'package:repaint_mobile/features/common/presentation/widgets/settings_heading.dart';
+import 'package:repaint_mobile/features/common/presentation/widgets/settings_scaffold.dart';
+import 'package:repaint_mobile/features/common/presentation/widgets/settings_tile.dart';
+import 'package:repaint_mobile/features/common/presentation/widgets/wide_elevated_button.dart';
 
 @RoutePage()
 class OperatorBeaconSettingsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("ビーコン設定"),
-        centerTitle: true,
-        leading: FlatIconButton(
-          onPressed: context.popRoute,
-          icon: Icons.chevron_left,
+    return SettingsScaffold(
+      title: "ビーコン設定",
+      children: [
+        const SettingsHeading("ビーコンの設定"),
+        const SizedBox(height: 16),
+        // TODO: ビーコンの名前変更処理を実装する
+        SettingsTile.textField(
+          label: "ビーコンの名前",
+          controller: TextEditingController(),
         ),
-        backgroundColor: Theme.of(context).colorScheme.background,
-      ),
-      backgroundColor: Theme.of(context).colorScheme.background,
+        const Spacer(),
+        Row(
+          children: [
+            Expanded(
+              child: WideElevatedButton(
+                onPressed: () {
+                  // TODO: ビーコンの登録解除処理を実装する
+                  context.popRoute();
+                },
+                text: "登録解除",
+                colors: const WideElevatedButtonColors(
+                  backgroundColor: Colors.white,
+                  borderColor: Colors.white,
+                  textColor: Colors.red,
+                ),
+              ),
+            ),
+            const SizedBox(width: 32.0),
+            Expanded(
+              child: WideElevatedButton(
+                onPressed: () {
+                  // TODO: ビーコンの登録処理を実装する
+                  context.popRoute();
+                },
+                text: "登録する",
+              ),
+            ),
+          ],
+        ),
+        const BottomConstrainedPadding(),
+      ],
     );
   }
 }
