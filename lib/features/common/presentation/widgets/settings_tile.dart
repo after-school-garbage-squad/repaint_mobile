c68569611b2804c072d1ba58e34cac00a36c62bd
Hideki Tanaka <work@tuxsnct.com>
2023-08-15 14:54:06 +0900

    refactor: put similar components in SettingsTile

--- lib/features/common/presentation/widgets/settings_tile.dart
+++ lib/features/common/presentation/widgets/settings_tile.dart
@@ -14,6 +14,54 @@ class SettingsTile extends StatelessWidget {
   final double? height;
   final EdgeInsets? padding;
 
+  factory SettingsTile.toggle({
+    required String title,
+    required bool value,
+    required ValueChanged<bool> onChanged,
+  }) {
+    return SettingsTile(
+      child: Row(
+        children: [
+          Text(title),
+          const Spacer(),
+          Switch(
+            value: value,
+            onChanged: onChanged,
+            activeTrackColor: const Color(0xff49e400),
+          )
+        ],
+      ),
+    );
+  }
+
+  factory SettingsTile.textField({
+    required String label,
+    required TextEditingController controller,
+    TextInputType? keyboardType,
+    void Function(String)? onChanged,
+  }) {
+    return SettingsTile(
+      child: TextField(
+        controller: controller,
+        keyboardType: keyboardType,
+        decoration: InputDecoration(
+          labelText: label,
+          floatingLabelBehavior: FloatingLabelBehavior.never,
+          suffixIcon: IconButton(
+            icon: const Icon(Icons.clear),
+            padding: EdgeInsets.zero,
+            onPressed: () => controller.clear(),
+          ),
+          suffixIconConstraints: BoxConstraints.tight(const Size.square(24.0)),
+          border: InputBorder.none,
+          contentPadding: const EdgeInsets.all(8.0),
+        ),
+        onChanged: onChanged,
+        scrollPhysics: const NeverScrollableScrollPhysics(),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
