ef28917b391337fdb49424dbf474d584e545ba06
Hideki Tanaka <work@tuxsnct.com>
2023-08-15 17:24:39 +0900

    feat: add size and shadowColor in FlatIconButton

--- lib/features/common/presentation/widgets/flat_icon_button.dart
+++ lib/features/common/presentation/widgets/flat_icon_button.dart
@@ -4,10 +4,12 @@ class FlatIconButtonColors {
   const FlatIconButtonColors({
     this.borderColor = Colors.transparent,
     this.backgroundColor = Colors.transparent,
+    this.shadowColor = Colors.transparent,
   });
 
   final Color? borderColor;
   final Color? backgroundColor;
+  final Color? shadowColor;
 }
 
 class FlatIconButton extends StatelessWidget {
@@ -16,13 +18,15 @@ class FlatIconButton extends StatelessWidget {
     required this.onPressed,
     required this.icon,
     this.iconSize = 32.0,
+    this.size = const Size.square(32.0),
     this.colors = const FlatIconButtonColors(),
     this.elevation = 0.0,
   });
 
   final VoidCallback onPressed;
   final IconData icon;
-  final double? iconSize;
+  final double iconSize;
+  final Size size;
   final FlatIconButtonColors? colors;
   final double? elevation;
 
@@ -33,12 +37,16 @@ class FlatIconButton extends StatelessWidget {
       onPressed: onPressed,
       icon: Icon(icon),
       iconSize: iconSize,
+      padding: EdgeInsets.zero,
+      constraints: BoxConstraints.tight(size),
       style: ButtonStyle(
         backgroundColor:
             MaterialStateProperty.all<Color>(colors!.backgroundColor!),
+        shadowColor: MaterialStateProperty.all<Color>(colors!.shadowColor!),
         shape: MaterialStateProperty.all<OutlinedBorder>(
           CircleBorder(side: BorderSide(color: colors!.borderColor!)),
         ),
+        fixedSize: MaterialStateProperty.all<Size>(size),
         elevation: MaterialStateProperty.all<double>(elevation!),
       ),
     );
