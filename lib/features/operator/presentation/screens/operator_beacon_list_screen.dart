1446ae74604e214363afab1a9b8babfaaa42b817
Hideki Tanaka <work@tuxsnct.com>
2023-08-16 12:05:22 +0900

    feat: OperatorBeaconListScreen almost completed

--- lib/features/operator/presentation/screens/operator_beacon_list_screen.dart
+++ lib/features/operator/presentation/screens/operator_beacon_list_screen.dart
@@ -1,28 +1,36 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
-import 'package:repaint_mobile/features/common/presentation/widgets/flat_icon_button.dart';
+import 'package:repaint_mobile/features/common/presentation/widgets/list_heading.dart';
+import 'package:repaint_mobile/features/common/presentation/widgets/list_scaffold.dart';
+import 'package:repaint_mobile/features/operator/presentation/widgets/operator_elevated_tile.dart';
 
 @RoutePage()
 class OperatorBeaconListScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("ビーコン一覧"),
-        centerTitle: true,
-        leading: Row(
-          children: [
-            const SizedBox(width: 16.0),
-            FlatIconButton(
-              onPressed: context.popRoute,
-              icon: Icons.chevron_left,
-            ),
-          ],
+    return ListScaffold(
+      children: [
+        const ListHeading("検知したビーコン"),
+        const SizedBox(height: 16),
+        OperatorElevatedTile.beacon(
+          onTap: () {},
+          title: "登録済みの名前",
+          distance: 10.0,
+          hwid: 0x0000000000,
         ),
-        leadingWidth: 64.0,
-        backgroundColor: Theme.of(context).colorScheme.background,
-      ),
-      backgroundColor: Theme.of(context).colorScheme.background,
+        OperatorElevatedTile.beacon(
+          onTap: () {},
+          title: "登録済みの名前",
+          distance: 10.0,
+          hwid: 0x0000000000,
+        ),
+        OperatorElevatedTile.beacon(
+          onTap: () {},
+          title: "登録済みの名前",
+          distance: 10.0,
+          hwid: 0x0000000000,
+        )
+      ],
     );
   }
 }
