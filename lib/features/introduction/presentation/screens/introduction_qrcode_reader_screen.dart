9cf2345af35972b350624039fd59d26cc7c24fe6
Hideki Tanaka <work@tuxsnct.com>
2023-09-03 23:17:06 +0900

    feat: remove unnecessary code for debugging

--- lib/features/introduction/presentation/screens/introduction_qrcode_reader_screen.dart
+++ lib/features/introduction/presentation/screens/introduction_qrcode_reader_screen.dart
@@ -2,20 +2,31 @@ import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:mobile_scanner/mobile_scanner.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:repaint_mobile/config/app_router.dart';
+import 'package:repaint_mobile/config/providers.dart';
+import 'package:repaint_mobile/features/common/domain/entities/user_entity.dart';
 import 'package:repaint_mobile/features/common/presentation/widgets/camera_scaffold.dart';
-import 'package:repaint_mobile/features/introduction/providers/providers.dart';
+import 'package:repaint_mobile/features/common/presentation/widgets/wide_elevated_button.dart';
 
 @RoutePage()
 class IntroductionQRCodeReaderScreen extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final controller =
-        ref.watch(introductionQRCodeReaderControllerProvider.future);
-
     return CameraScaffold(
-      preview: MobileScanner(
-        onDetect: (capture) async =>
-            (await controller).onQRCodeScanned(context, capture),
+      preview: Expanded(
+        child: MobileScanner(
+          onDetect: (capture) async {
+            // TODO: QRコードの内容を取得した際の処理を実装する
+            await ref.read(userProvider.notifier).setType(UserType.visitor);
+            if (context.mounted) {
+              await context.router.pushAndPopUntil(
+                const VisitorHomeRoute(),
+                predicate: (_) => false,
+              );
+            }
+          },
+        ),
       ),
       children: [
         Text(
@@ -25,8 +36,16 @@ class IntroductionQRCodeReaderScreen extends ConsumerWidget {
               .bodyLarge
               ?.copyWith(overflow: TextOverflow.ellipsis),
         ),
-        const SizedBox(height: 16.0),
-        const Text('カメラが起動しない場合は、アプリを再起動してください'),
+        const SizedBox(height: 20.0),
+        const Text('カメラが起動しない場合は、設定から使用許可をお願いします'),
+        const SizedBox(height: 20.0),
+        const WideElevatedButton(
+          onPressed: openAppSettings,
+          text: "設定に進む",
+          colors: WideElevatedButtonColors(
+            backgroundColor: Colors.white,
+          ),
+        ),
       ],
     );
   }
