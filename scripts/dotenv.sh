b7c99c958ef122079d81ede81d280e18ae5b6a51
Hideki Tanaka <work@tuxsnct.com>
2023-09-25 01:43:43 +0900

    chore: update scripts

--- scripts/dotenv.sh
+++ scripts/dotenv.sh
@@ -1,8 +1,4 @@
 #!/usr/bin/env bash
 set -ex
 touch .env
-(
-  echo "API_BASE_URL=$API_BASE_URL"
-  echo "SENTRY_DSN=$SENTRY_DSN"
-  echo "SENTRY_ENVIRONMENT=$SENTRY_ENVIRONMENT"
-) >> .env
+echo "API_BASE_URL=$API_BASE_URL" >> .env
