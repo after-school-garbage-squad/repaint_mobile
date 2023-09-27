3742b2ed61e0ec759d987cf0cb666348c699e5c8
Hideki Tanaka <work@tuxsnct.com>
2023-09-28 02:23:09 +0900

    chore: update local_properties.sh

--- scripts/local_properties.sh
+++ scripts/local_properties.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -ex
 (
+  # shellcheck disable=SC2028
+  echo "\n"
   echo "AUTH0_DOMAIN=$AUTH0_DOMAIN"
   echo "AUTH0_SCHEME=$AUTH0_SCHEME"
-) >> android/local.properties
+) >> ./android/local.properties
