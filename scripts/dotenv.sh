05521c2d8833322fa46c1b3f334a6f0241906a9b
Hideki Tanaka <work@tuxsnct.com>
2023-09-28 02:44:34 +0900

    chore: bump build number from 38 to 39

--- scripts/dotenv.sh
+++ scripts/dotenv.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 set -ex
-touch .env
 (
   echo "API_BASE_URL=$API_BASE_URL"
   echo "AUTH0_CLIENT_ID=$AUTH0_CLIENT_ID"
--- scripts/local_properties.sh
+++ scripts/local_properties.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 set -ex
 (
-  # shellcheck disable=SC2028
-  echo "\n"
   echo "AUTH0_DOMAIN=$AUTH0_DOMAIN"
   echo "AUTH0_SCHEME=$AUTH0_SCHEME"
-) >> ./android/local.properties
+) > ./android/local.properties
