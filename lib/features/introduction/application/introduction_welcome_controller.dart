693cb9764950635657c47816d82f72c844edb916
Hideki Tanaka <work@tuxsnct.com>
2023-09-27 23:57:50 +0900

    feat: change behavior of continue button in IntroductionWelcomeScreen

--- lib/features/introduction/application/introduction_welcome_controller.dart
+++ lib/features/introduction/application/introduction_welcome_controller.dart
@@ -1,12 +1,23 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:permission_handler/permission_handler.dart';
 import 'package:repaint_mobile/config/app_router.dart';
+import 'package:repaint_mobile/config/guards.dart';
 
 class IntroductionHomeController {
   const IntroductionHomeController();
 
-  void onContinuePressed(BuildContext context) {
-    context.pushRoute(const IntroductionStepperRoute());
+  Future<void> onContinuePressed(BuildContext context) async {
+    final futures = PermissionGuard.permissions
+        .map((element) => element.isGranted)
+        .toList();
+    await Future.wait(futures)
+        .then((value) => value.every((element) => element))
+        .then(
+          (value) => value
+              ? context.pushRoute(const IntroductionQRCodeReaderRoute())
+              : context.pushRoute(const IntroductionStepperRoute()),
+        );
   }
 
   void onSettingsPressed(BuildContext context) {
