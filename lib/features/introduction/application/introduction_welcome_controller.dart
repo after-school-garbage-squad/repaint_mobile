26d1d7b5a26973bef4c55b57e66df11c891b6160
Hideki Tanaka <work@tuxsnct.com>
2023-10-10 19:54:33 +0900

    style: fix analysis issues

--- lib/features/introduction/application/introduction_welcome_controller.dart
+++ lib/features/introduction/application/introduction_welcome_controller.dart
@@ -1,8 +1,6 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/cupertino.dart';
-import 'package:permission_handler/permission_handler.dart';
 import 'package:repaint_mobile/config/app_router.dart';
-import 'package:repaint_mobile/config/guards.dart';
 
 class IntroductionHomeController {
   const IntroductionHomeController();
