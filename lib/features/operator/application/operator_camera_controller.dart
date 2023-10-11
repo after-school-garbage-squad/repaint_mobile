33d97f219d1af845e1ed873baf3cb2379c99ee78
Hideki Tanaka <work@tuxsnct.com>
2023-10-11 17:04:56 +0900

    feat: add converting picture to png

--- lib/features/operator/application/operator_camera_controller.dart
+++ lib/features/operator/application/operator_camera_controller.dart
@@ -1,6 +1,9 @@
+import 'dart:io';
+
 import 'package:auto_route/auto_route.dart';
 import 'package:camera/camera.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:image/image.dart' as img;
 import 'package:logging/logging.dart';
 import 'package:path/path.dart';
 import 'package:path_provider/path_provider.dart';
@@ -21,10 +24,14 @@ class OperatorCameraController {
   ) async {
     _logger.info('picture taking...');
     final picture = await cameraController.takePicture();
+    final imageBytes = await picture.readAsBytes();
+    final bitmap = img.decodeImage(imageBytes);
     final directory = await getApplicationCacheDirectory();
     final path = join(directory.path, "${DateTime.now()}.png");
-    await picture.saveTo(path);
 
+    if (bitmap == null) return;
+
+    await File(path).writeAsBytes(img.encodePng(bitmap));
     _logger.info('picture taken: $path');
 
     if (context.mounted) {
